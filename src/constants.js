a694dd65730e2c411e4bf834784127cb0959efc4
madhaviporte <madhaviporte2004@gmail.com>
2026-05-21 01:52:33 +0530

    first commit

---
+++ src/constants.js
@@ -0,0 +1,301 @@
+// Skills Section Logo's
+import htmlLogo from './assets/tech_logo/html.png';
+import cssLogo from './assets/tech_logo/css.png';
+import sassLogo from './assets/tech_logo/sass.png';
+import javascriptLogo from './assets/tech_logo/javascript.png';
+import reactjsLogo from './assets/tech_logo/reactjs.png';
+import angularLogo from './assets/tech_logo/angular.png';
+import reduxLogo from './assets/tech_logo/redux.png';
+import nextjsLogo from './assets/tech_logo/nextjs.png';
+import tailwindcssLogo from './assets/tech_logo/tailwindcss.png';
+import gsapLogo from './assets/tech_logo/gsap.png';
+import materialuiLogo from './assets/tech_logo/materialui.png';
+import bootstrapLogo from './assets/tech_logo/bootstrap.png';
+import springbootLogo from './assets/tech_logo/springboot.png';
+import nodejsLogo from './assets/tech_logo/nodejs.png';
+import expressjsLogo from './assets/tech_logo/express.png';
+import mysqlLogo from './assets/tech_logo/mysql.png';
+import mongodbLogo from './assets/tech_logo/mongodb.png';
+import firebaseLogo from './assets/tech_logo/firebase.png';
+import cLogo from './assets/tech_logo/c.png';
+import cppLogo from './assets/tech_logo/cpp.png';
+import javaLogo from './assets/tech_logo/java.png';
+import pythonLogo from './assets/tech_logo/python.png';
+import typescriptLogo from './assets/tech_logo/typescript.png';
+import gitLogo from './assets/tech_logo/git.png';
+import githubLogo from './assets/tech_logo/github.png';
+import vscodeLogo from './assets/tech_logo/vscode.png';
+import postmanLogo from './assets/tech_logo/postman.png';
+import mcLogo from './assets/tech_logo/mc.png';
+import figmaLogo from './assets/tech_logo/figma.png';
+import netlifyLogo from './assets/tech_logo/netlify.png';
+import vercelLogo from './assets/tech_logo/vercel.png';
+import postgreLogo from './assets/tech_logo/postgre.png';
+import csharpLogo from './assets/tech_logo/csharp.png';
+
+// Experience Section Logo's
+import webverseLogo from './assets/company_logo/webverse_logo.png';
+import agcLogo from './assets/company_logo/agc_logo.png';
+import newtonschoolLogo from './assets/company_logo/newtonschool_logo.png';
+
+// Education Section Logo's
+import glaLogo from './assets/education_logo/gla_logo.png';
+import bsaLogo from './assets/education_logo/bsa_logo.png';
+import vpsLogo from './assets/education_logo/vps_logo.png';
+
+// Project Section Logo's
+import githubdetLogo from './assets/work_logo/github_det.png';
+import csprepLogo from './assets/work_logo/cs_prep.png';
+import movierecLogo from './assets/work_logo/movie_rec.png';
+import taskremLogo from './assets/work_logo/task_rem.png';
+import npmLogo from './assets/work_logo/npm.png';
+import webverLogo from './assets/work_logo/web_dig.png';
+import cmLogo from './assets/work_logo/cm.png';
+import imagesearchLogo from './assets/work_logo/image_search.png';
+import removebgLogo from './assets/work_logo/remove_bg.png';
+
+
+export const SkillsInfo = [
+  {
+    title: 'Frontend',
+    skills: [
+      { name: 'HTML', logo: htmlLogo },
+      { name: 'CSS', logo: cssLogo },
+      { name: 'SASS', logo: sassLogo },
+      { name: 'JavaScript', logo: javascriptLogo },
+      { name: 'React JS', logo: reactjsLogo },
+      { name: 'Angular', logo: angularLogo },
+      { name: 'Redux', logo: reduxLogo },
+      { name: 'Next JS', logo: nextjsLogo },
+      { name: 'Tailwind CSS', logo: tailwindcssLogo },
+      { name: 'GSAP', logo: gsapLogo },
+      { name: 'Material UI', logo: materialuiLogo },
+      { name: 'Bootstrap', logo: bootstrapLogo },
+    ],
+  },
+  {
+    title: 'Backend',
+    skills: [
+      { name: 'Springboot', logo: springbootLogo },
+      { name: 'Node JS', logo: nodejsLogo },
+      { name: 'Express JS', logo: expressjsLogo },
+      { name: 'MySQL', logo: mysqlLogo },
+      { name: 'MongoDB', logo: mongodbLogo },
+      { name: 'Firebase', logo: firebaseLogo },
+      { name: 'PostgreSQL', logo: postgreLogo },
+    ],
+  },
+  {
+    title: 'Languages',
+    skills: [
+      { name: 'C', logo: cLogo },
+      { name: 'C++', logo: cppLogo },
+      { name: 'Java', logo: javaLogo },
+      { name: 'Python', logo: pythonLogo },
+      { name: 'C-Sharp', logo: csharpLogo },
+      { name: 'JavaScript', logo: javascriptLogo },
+      { name: 'TypeScript', logo: typescriptLogo },
+    ],
+  },
+  {
+    title: 'Tools',
+    skills: [
+      { name: 'Git', logo: gitLogo },
+      { name: 'GitHub', logo: githubLogo },
+      { name: 'VS Code', logo: vscodeLogo },
+      { name: 'Postman', logo: postmanLogo },
+      { name: 'Compass', logo: mcLogo },
+      { name: 'Vercel', logo: vercelLogo },
+      { name: 'Netlify', logo: netlifyLogo },
+      { name: 'Figma', logo: figmaLogo },
+    ],
+  },
+];
+
+  export const experiences = [
+    {
+      id: 0,
+      img: webverseLogo,
+      role: "Frontend Designer (Member)",
+      company: "SHERYIANS HACKATHON 2026",
+      date: "1-Week(Online Mode)",
+      desc: "Built a full-stack customer support platform where users can create support tickets, chat with AI, and communicate with human agents in real time. Agents can manage and respond to assigned tickets, while admins handle users, knowledge base content, and platform management. The application is built using React, Node.js, Express, and MongoDB, with optional Redis caching for better performance. Google Gemini integration powers AI-based responses and smart assistance features.",
+      skills: [
+        "API Integration","Real-Time Communication" , 
+"State Management" , 
+ "Responsive Design" , 
+" Debugging & Testing",  
+" Git & GitHub Collaboration ", 
+ "REST API Development"  ,
+ "Database Management" , 
+ "Performance Optimization" , 
+ "Agile Teamwork"
+      ],
+    },
+    // {
+    //   id: 1,
+    //   img: agcLogo,
+    //   role: "Fullstack Engineer",
+    //   company: "Agumentik Group of Companies",
+    //   date: "July 2023 - March 2024",
+    //   desc: "Contributed to innovative projects as a Fullstack Engineer, leading both frontend and backend development using technologies such as HTML, CSS, JavaScript, PHP, SQL, Bootstrap, and ReactJS. Worked closely with the team to deliver responsive, high-performance web applications and improve user experience through seamless integration of various technologies.",
+    //   skills: [
+    //     "ReactJS",
+    //     "Redux",
+    //     "JavaScript",
+    //     "Tailwind CSS",
+    //     "HTML",
+    //     "CSS",
+    //     "SQL",
+    //   ],
+    // },
+    // {
+    //   id: 2,
+    //   img: newtonschoolLogo,
+    //   role: "Frontend Intern",
+    //   company: "Newton School",
+    //   date: "September 2021 - August 2022",
+    //   desc: "Worked as a Frontend Developer Intern, designing and implementing scalable UI components and responsive websites using HTML, CSS, JavaScript, Bootstrap, and Material UI. Collaborated with the design team to translate wireframes and prototypes from Figma into interactive, user-friendly web pages.",
+    //   skills: [
+    //     "HTML",
+    //     "CSS",
+    //     "Javascript",
+    //     "Bootstrap",
+    //     "Figma",
+    //     "Material UI",
+    //   ],
+    // },
+  ];
+  
+  export const education = [
+    {
+      id: 0,
+      img: bsaLogo,
+      school: "Indian Institute of Information Technology Ranchi",
+      date: "Aug 2023 - May 2027",
+      grade: "78.8%",
+      desc: "I am currently pursuing my Bachelor of Technology (B.Tech) in Computer Science and Engineering from the Indian Institute of Information Technology Ranchi. I joined in 2023 and my expected graduation year is 2027. During my college journey, I am learning core computer science subjects like Data Structures and Algorithms, Web Development, Database Management Systems, and Object-Oriented Programming. I am gradually building my understanding of how software development works in real-world scenarios. Along with academics, I also try to work on projects to apply what I learn and improve my development skills.",
+      degree: "Bachelor of Technology (B.Tech) - Computer Science and Engineering",
+    },
+    {
+      id: 1,
+      img: glaLogo,
+      school: "Sheriyans Coding School",
+      date: "Sept 2025 - June 2026",
+      grade: "In Progress",
+      desc: "Completed the Job Ready AI-Powered Cohort 2.0, an intensive training program focused on becoming a job-ready software engineer. The program covered MERN stack development, Data Structures and Algorithms (DSA), and aptitude training. Through this cohort, I gained hands-on experience in building full-stack applications, improving problem-solving skills, and strengthening my overall technical foundation for placements and real-world development.",
+      degree: "Cohort 2.0 - MERN Stack Development, DSA & Aptitude Training",
+    },
+    {
+      id: 2,
+      img: vpsLogo,
+      school: "DAV Public School",
+      date: "Apr 2020 - March 2021",
+      grade: "78.8%",
+      desc: "I completed my class 12 education from DAV Public School, under the CBSE board, where I studied Physics, Chemistry, and Mathematics (PCM) with Fundamental Computer Science.",
+      degree: "CBSE(XII) - PCM with Fundamental Computer Science",
+    },
+    {
+      id: 3,
+      img: vpsLogo,
+      school: "DAV Public School",
+      date: "Apr 2018 - March 2019",
+      grade: "66.8%",
+      desc: "I completed my Class 10 education from DAV Public School under the CBSE board, where I studied Science, Social Science, Mathematics, Hindi, and English.",
+      degree: "CBSE(X)",
+    },
+  ];
+  
+  export const projects = [
+    {
+      id: 0,
+      title: "3D Interactive Dog Web App",
+      description:
+        "Built an interactive 3D dog experience in the browser using React and Three.js. The project includes smooth real-time rendering and animations to create a more engaging user experience. I also added basic user interactions so the 3D model responds on screen, and focused on optimizing performance to ensure smooth, lag-free rendering. The application is deployed on Vercel.",
+      image: githubdetLogo,
+      tags: ["ReactJS", "ThreeJs","JavaScript", "SCSS"],
+      github: "https://github.com/madhaviporte/3D-DOG",
+      webapp: "https://3-d-dog-ten.vercel.app/",
+    },
+    {
+      id: 1,
+      title: "Productivity DashBoard",
+      description:
+        "Built a multi-feature productivity dashboard to manage daily tasks and activities. It includes sections like To-Do List, Daily Planner, Pomodoro Timer, and Daily Goals. The UI is interactive, allowing users to switch between different tools easily. I used JavaScript for DOM manipulation and real-time user interactions, and also integrated APIs to show dynamic data like weather updates.",
+      image: csprepLogo,
+      tags: ["HTML", "CSS", "JavaScript", "APIs"],
+      github: "https://github.com/madhaviporte/COHORT2-PROJECT/tree/main/productivity-dashboard",
+      webapp: "https://productivity-databoard.vercel.app/",
+    },
+    {
+      id: 2,
+      title: "Mac OS Style Portfolio",
+      description:
+        "Designed a personal portfolio inspired by the macOS interface and layout. It features interactive UI elements like app-style windows and a smooth navigation system. The focus was on clean design and a seamless user experience. The project is deployed on Vercel for live access.",
+      image: movierecLogo,
+      tags: ["React JS", "JavaScript", "SCSS"],
+      github: "https://github.com/madhaviporte/COHORT2-PROJECT/tree/main/React-Resume",
+      webapp: "https://cohort-2-project.vercel.app/",
+    },
+    {
+      id: 3,
+      title: "Animated React website",
+      description:
+        "I created this project to practice building modern UI with animations and better user experience. The website includes multiple sections with smooth navigation using React Router. I focused on keeping the design clean, interactive, and responsive across all devices.",
+      image: taskremLogo,
+      tags: ["React.js","Tailwind CSS","React Router DOM","JavaScript (ES6+)","HTML5",
+"CSS3"],
+      github: "https://github.com/madhaviporte/COHORT2-PROJECT/tree/main/Landing-Page/Animated%20website",
+      webapp: "https://animated-website-green.vercel.app/",
+    },
+    {
+      id: 4,
+      title: "Perplexity",
+      description:
+        "Built a full-stack AI chat application using React, Node.js, Express, and MongoDB with secure JWT-based authentication. Developed a real-time chat interface with instant UI updates for a smooth user experience. Implemented context-aware AI responses by using chat history to improve response accuracy. Designed REST APIs and optimized backend performance to ensure fast response times (1–2 seconds). Integrated MongoDB for persistent chat storage and email services for notifications.",
+      image: npmLogo,
+      tags: ["React JS","Tailwind CSS", "Node.js","ExpressJs","MongoDB","JWT","Postman","AI APIs","SMTP/Email"],
+      github: "https://github.com/madhaviporte/Perplexity",
+      // webapp: "https://www.npmjs.com/package/cmtk-email-validator",
+    },
+    {
+      id: 5,
+      title: "Moodify",
+      description:
+        "Built a full-stack music recommendation app that uses real-time facial expression detection to suggest songs based on user mood. Integrated camera input with backend APIs to enable seamless mood detection and instant responses. Implemented secure JWT authentication with Redis token blacklisting to enhance session security. Used Redis caching to optimize performance, reducing response time and improving user experience. Designed scalable REST APIs and handled media processing to ensure smooth image handling and music playback.",
+      image: webverLogo,
+      tags: ["Javascript", "ReactJs"," NodeJs", "ExpressJs", "MongoDB", "Redis", "ImageKit", "Postman"],
+      github: "https://github.com/madhaviporte/Moodify",
+      // webapp: "https://webversedigital.com/",
+    },
+    {
+      id: 6,
+      title: "KORA AI-User & Operator Manual",
+      description:
+        "This guide explains Kohra AI in plain language: what it is, how to run it, and how to use it day to day. Kohra AI is a multi-tenant customer support platform: each business (tenant) has its own data, and users belong to one tenant.",
+      image: cmLogo,
+      tags: ["React JS", "Node.js", "MongoDB", "Express", "Payment Integration"],
+      github: "https://github.com/madhaviporte/KORA",
+      webapp: "https://kora-virid.vercel.app/",
+    },
+    {
+      id: 7,
+      title: "SNITCH-ECommerce Web App",
+      description:
+        "Built a modern full-stack eCommerce web application using the MERN stack, allowing users to explore products, add items to cart, place orders, and manage their accounts. The application includes secure authentication, an admin panel for product and order management, payment integration, and a fully responsive UI to ensure a smooth shopping experience across all devices.",
+      image: imagesearchLogo,
+      tags: ["ReactJS","CSS","Redux","NodeJS+ExpressJs","MongoDB","Mongoose","Razorpay","Axios","JWT+Cookies/Sessions"],
+      github: "https://github.com/codingmastr/Image-Search-App",
+      webapp: "https://vercel.com/madhavis-projects-d069168b/cohort-2",
+    },
+    // {
+    //   id: 8,
+    //   title: "Image Background Remover",
+    //   description:
+    //     "An efficient background removal app built with React.js and API integration. Users can upload any image, remove the background, and download the transparent version for further use.",
+    //   image: removebgLogo,
+    //   tags: ["React JS", "API", "Image Processing", "HTML", "CSS", "Javascript"],
+    //   github: "https://github.com/codingmastr/Image-Background-Remover",
+    //   webapp: "https://removeyourbg.netlify.app/",
+    // },
+  ];  
