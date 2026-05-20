5d2b2345e4381748c365c2f584422a680a2da3a4
madhaviporte <madhaviporte2004@gmail.com>
2026-05-21 02:13:50 +0530

    image position fix

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -76,7 +76,7 @@ const About = () => {
             <img
               src={profileImage}
               alt="Madhavi Porte"
-              className="w-full h-full rounded-full object-cover drop-shadow-[0_10px_20px_rgba(130,69,236,0.5)]"
+              className="w-full h-full rounded-full object-cover object-top drop-shadow-[0_10px_20px_rgba(130,69,236,0.5)]"
             />
           </Tilt>
         </div>
