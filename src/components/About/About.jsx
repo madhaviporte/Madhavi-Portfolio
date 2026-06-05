3644db5938605e81562111b5440f0345daef6030
madhaviporte <madhaviporte2004@gmail.com>
2026-06-06 00:08:56 +0530

    update resume link in drive

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -49,7 +49,7 @@ const About = () => {
           </p>
           {/* Resume Button */}
           <a
-            href="https://drive.google.com/file/d/1kAMJoaDR1_AUlLGb3SdJgX7_MHnbMWf8/view?usp=drivesdk"
+            href="https://drive.google.com/file/d/1r9VfdBgi7rgpTcRtQAO9XON6bGGcxq6n/view?usp=drivesdk"
             target="_blank"
             rel="noopener noreferrer"
             className="inline-block text-white py-3 px-8 rounded-full mt-5 text-lg font-bold transition duration-300 transform hover:scale-105"
